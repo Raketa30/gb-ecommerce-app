1088828449e938814a71363859788bb2ff4a08f1
duckpool <duckpool@yandex.ru>
2021-10-09 15:56:43 +0300

    fix

---
+++ cloud-eureka-server/src/test/java/com/gb/ecomm/eureka/CloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.ecomm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
